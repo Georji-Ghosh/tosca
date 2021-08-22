be4023d8021637c94606a64218ffa6b160ec894e
Georji Ghosh <georji.ghosh@gmail.com>
2021-08-22 12:55:41 +0530

    Initial setup done

---
+++ tosca-backend/src/main/java/org/orchestration/tosca/ToscaApplication.java
@@ -0,0 +1,13 @@
+package org.orchestration.tosca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToscaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToscaApplication.class, args);
+	}
+
+}
